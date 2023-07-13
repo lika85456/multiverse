8888577a1fdda192e9f177906abe5261d96820a2
lika85456 <lika85456@post.cz>
2023-07-13 18:42:17 +0200

    fix: WIP database deploy

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -2,7 +2,7 @@ import type { Options } from "tsup";
 
 export const tsup: Options = {
     bundle: true,
-    skipNodeModulesBundle: false,
-    noExternal: [/./],
+    // skipNodeModulesBundle: false,
+    // noExternal: [/./],
     minify: true
 };
