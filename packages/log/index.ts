144a439c56cbb05bca2fa997eb7c7fa167427fc2
lika85456 <lika85456@post.cz>
2023-08-27 14:17:43 +0200

    feat(log): new logger

---
+++ packages/log/index.ts
@@ -0,0 +1,8 @@
+import type { ILogObj } from "tslog";
+import { Logger } from "tslog";
+
+const log: Logger<ILogObj> = new Logger();
+
+// https://tslog.js.org/#/?id=simple-example
+
+export default log;
