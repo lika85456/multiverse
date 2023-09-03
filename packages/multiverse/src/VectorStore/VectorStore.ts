fed1a962315e5bd79a926e03fcf5a2849e466193
lika85456 <lika85456@post.cz>
2023-09-03 11:22:01 +0200

    refactor: removed VectorStore

--- packages/multiverse/src/VectorStore/VectorStore.ts
+++
@@ -1,9 +0,0 @@
-import type { Partition, StoredVector } from "../Database/Vector";
-
-export default interface VectorStore {
-    add(vectors: StoredVector[]): Promise<void>;
-    remove(ids: number[]): Promise<void>;
-    getByLabel(label: string): Promise<StoredVector | undefined>;
-    partition(partition: Partition): AsyncGenerator<StoredVector, void, unknown>;
-    changesAfter(timestamp: number, partition: Partition): AsyncGenerator<StoredVector, void, unknown>;
-}
