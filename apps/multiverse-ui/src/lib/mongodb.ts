cb0849224c86078e3397e6e163b4aec7d41cb27c
kornumic <kornumic@gmail.com>
2024-03-21 18:49:52 +0100

    ts-ignore in copied mognodb client

--- apps/multiverse-ui/src/lib/mongodb.ts
+++ apps/multiverse-ui/src/lib/mongodb.ts
@@ -11,10 +11,13 @@ let clientPromise: Promise<MongoClient>;
 if (ENV.NODE_ENV === "development") {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
+    // @ts-ignore
     if (!global._mongoClientPromise) {
         client = new MongoClient(uri, options);
+        // @ts-ignore
         global._mongoClientPromise = client.connect();
     }
+    // @ts-ignore
     clientPromise = global._mongoClientPromise;
 } else {
     // In production mode, it's best to not use a global variable.
