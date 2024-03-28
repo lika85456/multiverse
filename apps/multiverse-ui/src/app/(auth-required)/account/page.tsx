5823d03d4a4134b73cb90080759e27551d41ed5b
kornumic <kornumic@gmail.com>
2024-03-28 23:02:01 +0100

    Added AddAwsTokenModal with simple form

--- apps/multiverse-ui/src/app/(auth-required)/account/page.tsx
+++ apps/multiverse-ui/src/app/(auth-required)/account/page.tsx
@@ -1,13 +1,15 @@
 import AWSToken from "@/features/account/AWSToken";
 import PageTitle from "@/app/layout/components/PageTitle";
 import { Separator } from "@/components/ui/separator";
+import { getSessionUser } from "@/lib/mongodb/collections/user";
 
-export default function Account() {
-    const userName = "Michal Kornúc";
+export default async function Account() {
+    const sessionUser = await getSessionUser();
+    if (!sessionUser) return null;
 
     return (
         <>
-            <PageTitle title={`${userName} - account`} />
+            <PageTitle title={`${sessionUser.name} - account`} />
             <Separator className="bg-border m-4" />
             <AWSToken />
         </>
--- apps/multiverse-ui/src/features/account/AddAWSTokenModal.tsx
+++ apps/multiverse-ui/src/features/account/AddAWSTokenModal.tsx
@@ -1,17 +1,54 @@
 import { trpc } from "@/_trpc/client";
-import { IoAdd } from "react-icons/io5";
+import {
+    IoAdd, IoCheckmark, IoClose
+} from "react-icons/io5";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import useModal from "@/features/modals/use-modal";
+import {
+    AlertDialog,
+    AlertDialogContent, AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger
+} from "@/components/ui/alert-dialog";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form, FormControl, FormField, FormItem, FormLabel, FormMessage
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+
+const AwsTokenSchema = z.object({
+    accessTokenId: z.string().min(8).max(256),
+    secretAccessKey: z.string().min(16).max(256),
+});
 
 export default function AddAWSTokenModal() {
     const refetchToken = trpc.useUtils().getAwsToken.refetch;
     const mutation = trpc.addAwsToken.useMutation();
 
-    const handleAddAwsToken = async() => {
+    const {
+        modalOpen, handleOpenModal, handleCloseModal
+    } = useModal();
+
+    const form = useForm<z.infer<typeof AwsTokenSchema>>({
+        resolver: zodResolver(AwsTokenSchema),
+        defaultValues: {
+            accessTokenId: "",
+            secretAccessKey: "",
+        }
+        // defaultValues: {
+        //     accessTokenId: "9daff09a7328358dfa44cdb321c38df3",
+        //     secretAccessKey: "2ed1487f8d68b49a5ca24e0657d2e21e54d7e2e942b3ab9ff485404da7c8912bddca20babe337c0736b6974f875e972410e2ad9800188f7c426225cf2cf30bd1",
+        // }
+    });
+
+    async function onSubmit(values: z.infer<typeof AwsTokenSchema>) {
         const result = await mutation.mutateAsync({
-            accessTokenId: "44b2d438d65261a9eb14f61b215a13e974342afe8f5d811",
-            secretAccessKey:
-        "c975e5a171bcde0588a51e2d7a5d6b2e7696a38639aba7f92724dedaa37b6c4c650d9f7f72e174314c172874f4314e5a746066c26c35c9631",
+            accessTokenId: values.accessTokenId,
+            secretAccessKey: values.secretAccessKey,
         });
         if (result) {
             await refetchToken();
@@ -19,15 +56,85 @@ export default function AddAWSTokenModal() {
         } else {
             toast("Error adding AWS Token");
         }
-    };
+    }
 
     return (
-        <Button
-            className="self-center flex w-fit bg-accent text-accent-foreground hover:bg-accent_light"
-            onClick={handleAddAwsToken}
-        >
-            <IoAdd className="w-6 h-6 mr-2" />
-      Add AWS Token
-        </Button>
+        <AlertDialog open={modalOpen}>
+            <AlertDialogTrigger
+                asChild
+                className="flex w-fit"
+                onClick={handleOpenModal}
+            >
+                <Button
+                    className="self-center flex w-fit bg-accent text-accent-foreground hover:bg-accent_light"
+                    onClick={handleOpenModal}
+                >
+                    <IoAdd className="w-6 h-6 mr-2" />
+                    Add AWS Token
+                </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent className="bg-card border-0">
+                <Form {...form}>
+                    <AlertDialogHeader>
+                        <div className="flex flex-row justify-between">
+                            <AlertDialogTitle>Set AWS token</AlertDialogTitle>
+                            <IoClose
+                                onClick={handleCloseModal}
+                                className="w-4 h-4 cursor-pointer hover:text-secondary-foreground transition-all "
+                            />
+                        </div>
+                    </AlertDialogHeader>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                            control={form.control}
+                            name="accessTokenId"
+                            render={({ field }) => (
+
+                                <FormItem>
+                                    <FormLabel>Access Token Id</FormLabel>
+                                    <FormControl>
+                                        <Textarea placeholder="Access Token Id" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="secretAccessKey"
+                            render={({ field }) => (
+
+                                <FormItem>
+                                    <FormLabel>Secret Access Key</FormLabel>
+                                    <FormControl>
+                                        <Textarea placeholder="Secret Access Key" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+
+                            )}
+                        />
+                        <AlertDialogFooter className="pt-8">
+                            <Button
+                                className="flex w-full bg-inherit hover:bg-primary border border-border"
+                                onClick={handleCloseModal}
+                            >
+                                <IoClose className="w-6 h-6 mr-2" />
+                            Cancel
+                            </Button>
+                            <Button
+                                type="submit"
+                                className="flex w-full text-accent-foreground bg-accent hover:bg-accent_light"
+                            >
+                                <IoCheckmark className="w-6 h-6 mr-2" />
+                            Add Token
+                            </Button>
+                        </AlertDialogFooter>
+                    </form>
+                </Form>
+            </AlertDialogContent>
+        </AlertDialog>
+
     );
 }
