df56943db6d26e8d996ea533bab5964880b44466
kornumic <kornumic@gmail.com>
2024-04-05 17:36:13 +0200

    Fixed useDateInterval() with error in date interval validation

--- apps/multiverse-ui/src/features/statistics/use-date-interval.ts
+++ apps/multiverse-ui/src/features/statistics/use-date-interval.ts
@@ -13,47 +13,54 @@ export default function useDateInterval() {
         from: addDays(new Date(), -7),
         to: new Date(),
     };
-    const currentDate = new Date();
 
     const [date, setDate] = React.useState<DefinedDateRange>(defaultInterval,);
     const handleDateIntervalChange = (newDate: DateRange | undefined) => {
-        if (!newDate) {
-            toast("No date interval selected. Displaying last 7 days.");
-            setDate(defaultInterval);
-
-            return;
-        } else if (
-            newDate.to &&
-      newDate.from &&
-      newDate.to.getDate() > currentDate.getDate()
-        ) {
-            console.log("debug", newDate.to);
-            toast("Interval had been trimmed to the current date.");
-            if (
-                newDate.from > currentDate ||
-        newDate.from.getDate() > newDate.to.getDate()
-            ) {
-                setDate({
-                    from: currentDate,
-                    to: currentDate,
-                });
-
-                return;
-            }
-            setDate({
-                from: newDate.from,
-                to: currentDate,
-            });
-
-            return;
-        } else if (!newDate.to || !newDate.from) {
+        const currentDate = new Date();
+        // If the date interval is not properly defined, set the default interval
+        if (!newDate || !newDate.from || !newDate.to) {
             toast("Invalid date interval. Displaying last 7 days.");
             setDate(defaultInterval);
 
             return;
         }
-    };
+        let from = newDate.from;
+        let to = newDate.to;
 
+        // Check flip reversed interval bounds
+        if (from > to) {
+            const tmp = from;
+            from = to;
+            to = tmp;
+        }
+
+        // Check if the interval is in the future
+        if (to > currentDate) {
+            // Check from bound of the interval
+            if (from > currentDate) {
+                toast("Invalid date interval. Displaying last 7 days.");
+                setDate(defaultInterval);
+
+                return;
+            }
+            // From is valid, trim to bound to the current date
+            toast("Interval had been trimmed to the current date.");
+            setDate({
+                from: from,
+                to: currentDate,
+            });
+
+            return;
+        }
+        // TODO - Check if the interval is too short
+        // TODO - Check if the interval is too long
+
+        // Set the new date interval without changes
+        setDate({
+            from: from,
+            to: to,
+        });
+    };
 
     return {
         date,
