dc0cd31809889c82f37b2efff553a3a2516b96ea
kornumic <kornumic@gmail.com>
2024-04-12 17:04:09 +0200

    Moved sqsHandler out of loop

--- apps/multiverse-ui/src/app/api/statistics/route.ts
+++ apps/multiverse-ui/src/app/api/statistics/route.ts
@@ -4,13 +4,13 @@ import { getAllQueuesWithCredentials } from "@/lib/mongodb/collections/user";
 export async function POST() {
     const queuesWithCredentials = await getAllQueuesWithCredentials();
 
+    const sqsHandler = new SQSHandler();
     const messages = await Promise.all(queuesWithCredentials.map(async(queue) => {
         const queueName = queue.sqs;
         const awsToken = {
             accessTokenId: queue.accessKeyId,
             secretAccessKey: queue.secretAccessKey
         };
-        const sqsHandler = new SQSHandler();
         const messages = await sqsHandler.receiveMessages(queueName, awsToken);
 
         //TODO process messages
