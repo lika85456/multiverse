134eb024e91744511375edae80be5c328e136cc7
kornumic <kornumic@gmail.com>
2024-04-13 14:59:04 +0200

    Processing general statistics

--- apps/multiverse-ui/src/features/statistics/statistics-processor/StatisticsProcessor.ts
+++ apps/multiverse-ui/src/features/statistics/statistics-processor/StatisticsProcessor.ts
@@ -1,7 +1,11 @@
-import type { Event, QueryEvent } from "@/features/statistics/statistics-processor/event";
+import type { Event } from "@/features/statistics/statistics-processor/event";
 import type { DailyStatisticsAdd } from "@/lib/mongodb/collections/daily-statistics";
 import { addDailyStatistics } from "@/lib/mongodb/collections/daily-statistics";
 import { getDailyStatistics } from "@/lib/mongodb/collections/daily-statistics";
+import {
+    addGeneralDatabaseStatistics,
+    getGeneralDatabaseStatistics
+} from "@/lib/mongodb/collections/general-database-statistics";
 
 export class StatisticsProcessor {
 
@@ -28,7 +32,87 @@ export class StatisticsProcessor {
         }, new Map<string, Event[]>());
     }
 
+    private calculateCost(databaseName: string, date: string): number {
+        console.log(`Calculating cost for database ${databaseName} and date ${date}`);
+
+        //TODO - calculate cost
+        return 0;
+    }
+
+    private extractWriteData(event: Event) {
+        if (event.type === "add") {
+            return {
+                timestamp: event.timestamp,
+                totalVectors: event.totalVectors,
+                dataSize: event.totalVectors * 500, //TODO use returned data when provided
+            };
+        } else if (event.type === "remove") {
+            return {
+                timestamp: event.timestamp,
+                totalVectors: event.totalVectors,
+                dataSize: event.totalVectors * 500, //TODO use returned data when provided
+            };
+        }
+
+        throw new Error("Could not extract write data from non-write event");
+    }
+
+    private applyGeneralStatistics(databaseName: string, events: Event[]): void {
+        // asynchronously update general statistics if the latest event is newer
+        getGeneralDatabaseStatistics(databaseName).then(async(generalStatistics) => {
+            // filter only write events (query events don't contain total vectors information)
+            const writeEvents = events.filter((event) => event.type === "add" || event.type === "remove");
+            if (writeEvents.length === 0) {
+                console.log(`No write events for database ${databaseName} received`);
+
+                return;
+            }
+
+            // at least one write event exists, find the latest one
+            const latestWriteEvent = writeEvents.reduce((acc, event) => {
+                if (acc.timestamp > event.timestamp) {
+                    return acc;
+                }
+
+                return event;
+            }, writeEvents[0]);
+
+            // extract total vectors and data size from the latest write event
+            const {
+                totalVectors, dataSize, timestamp
+            } = this.extractWriteData(latestWriteEvent);
+
+            // general statistics don't exist for this database, create new statistics
+            if (!generalStatistics) {
+                await addGeneralDatabaseStatistics({
+                    databaseName: databaseName,
+                    updated: new Date(timestamp),
+                    dataSize: dataSize,
+                    totalVectors: totalVectors,
+                });
+                console.log(`General statistics for database ${databaseName} created`);
+
+                return;
+            }
+            // general statistics exist for this database, update if the latest write event is newer
+            if (latestWriteEvent.timestamp > generalStatistics.updated.getTime()) {
+                generalStatistics.updated = new Date(timestamp);
+                generalStatistics.dataSize = dataSize;
+                generalStatistics.totalVectors = totalVectors;
+
+                await addGeneralDatabaseStatistics(generalStatistics);
+                console.log(`General statistics for database ${databaseName} updated`);
+
+                return;
+            }
+
+            // don't update otherwise
+            console.log(`General statistics for database ${databaseName} not updated`);
+        });
+    }
+
     private applyStatistics(events: Event[], innit: DailyStatisticsAdd): DailyStatisticsAdd {
+        // calculate statistics from events and set resulting daily statistics
         const result = events.reduce((acc, event) => {
             if (event.type === "add") {
                 acc.writeCount += 1;
@@ -36,8 +120,7 @@ export class StatisticsProcessor {
                 acc.writeCount += 1;
             } else if (event.type === "query") {
                 acc.readCount += 1;
-                const queryEvent = event as QueryEvent;
-                acc.totalResponseTime += queryEvent.duration;
+                acc.totalResponseTime += event.duration;
             }
 
             return acc;
@@ -45,13 +128,6 @@ export class StatisticsProcessor {
         result.totalCost = result.totalCost += this.calculateCost(innit.databaseName, innit.date);
 
         return result;
-    }
-
-    private calculateCost(databaseName: string, date: string): number {
-        console.log(`Calculating cost for database ${databaseName} and date ${date}`);
-
-        //TODO - calculate cost
-        return 0;
     }
 
     private async processEventsForDatabase(databaseName: string, events: Event[]): Promise<boolean> {
@@ -65,32 +141,41 @@ export class StatisticsProcessor {
 
             if (filteredStatistics.length === 0) {
                 // if there are no statistics for the date, create new statistics
-                const dailyStatistics = this.applyStatistics(events, {
+                const innit: DailyStatisticsAdd = {
                     date: date,
                     databaseName: databaseName,
                     writeCount: 0,
                     readCount: 0,
                     totalResponseTime: 0,
                     totalCost: 0
-                } as DailyStatisticsAdd);
-                addDailyStatistics(dailyStatistics).then(() => {
+                };
+                // apply statistics to the innit object
+                addDailyStatistics(this.applyStatistics(events, innit)).then(() => {
                     console.log(`Statistics for database ${databaseName} and date ${date} processed`);
                 });
+
+                //update general statistics
+                this.applyGeneralStatistics(databaseName, events);
+
             } else if (filteredStatistics.length === 1) {
                 // if there are statistics for the date, update existing statistics
                 const statistics = filteredStatistics[0];
-                const dailyStatistics = this.applyStatistics(events, {
+                const innit: DailyStatisticsAdd = {
                     date: date,
                     databaseName: databaseName,
                     writeCount: statistics.writeCount,
                     readCount: statistics.readCount,
                     totalResponseTime: statistics.totalResponseTime,
                     totalCost: statistics.totalCost
-                } as DailyStatisticsAdd);
-
-                addDailyStatistics(dailyStatistics).then(() => {
+                };
+                // apply statistics to the innit object constructed from the existing statistics
+                addDailyStatistics(this.applyStatistics(events, innit)).then(() => {
                     console.log(`Statistics for database ${databaseName} and date ${date} processed`);
                 });
+
+                //update general statistics
+                this.applyGeneralStatistics(databaseName, events);
+
             } else {
                 // if there are multiple statistics for the date, log an error
                 // don't throw an error to not block the processing of other databases
--- apps/multiverse-ui/src/lib/mongodb/collections/general-database-statistics.ts
+++ apps/multiverse-ui/src/lib/mongodb/collections/general-database-statistics.ts
@@ -1,4 +1,8 @@
-import { ObjectId } from "mongodb";
+import type { ObjectId } from "mongodb";
+import clientPromise from "@/lib/mongodb/mongodb";
+import { getDatabase } from "@/lib/mongodb/collections/database";
+
+export const collectionName = "general_database_statistics";
 
 export interface GeneralDatabaseStatisticsGet{
     _id: ObjectId;
@@ -7,39 +11,72 @@ export interface GeneralDatabaseStatisticsGet{
     // statistics data
     updated: Date; // for data freshness comparison
     dataSize: number;
-    totalRecords: number;
+    totalVectors: number;
     ownerId: ObjectId;
 }
 
 export interface GeneralDatabaseStatisticsInsert{
-    _id: ObjectId;
     // unique, identifies general statistics for a database
     databaseName: string;
     // statistics data
     updated: Date; // for data freshness comparison
-    dataSize?: number;
-    totalRecords?: number;
-    ownerId: ObjectId;
+    dataSize: number;
+    totalVectors: number;
 }
 
-export const getGeneralDatabaseStatistics = async(databaseName: string): Promise<GeneralDatabaseStatisticsGet> => {
-    return {
-        _id: new ObjectId(),
-        databaseName: "",
-        updated: new Date(),
-        dataSize: 0,
-        totalRecords: 0,
-        ownerId: new ObjectId()
-    };
+export const getGeneralDatabaseStatistics = async(databaseName: string): Promise<GeneralDatabaseStatisticsGet | undefined> => {
+
+    try {
+        const client = await clientPromise;
+        const db = client.db();
+
+        const result = await db.collection(collectionName).findOne({ databaseName });
+
+        if (!result) {
+            return undefined;
+        }
+
+        return {
+            _id: result._id,
+            databaseName: result.databaseName,
+            updated: result.updated,
+            dataSize: result.dataSize,
+            totalVectors: result.totalRecords,
+            ownerId: result.ownerId
+        };
+
+    } catch (error) {
+        throw new Error("Error getting general database statistics");
+    }
 };
 
-export const addGeneralDatabaseStatistics = async(statistics: GeneralDatabaseStatisticsInsert): Promise<GeneralDatabaseStatisticsGet> => {
-    return {
-        _id: new ObjectId(),
-        databaseName: "",
-        updated: new Date(),
-        dataSize: 0,
-        totalRecords: 0,
-        ownerId: new ObjectId()
-    };
+export const addGeneralDatabaseStatistics = async(statistics: GeneralDatabaseStatisticsInsert): Promise<void> => {
+    const ownerId = (await getDatabase(statistics.databaseName))?.ownerId;
+    if (!ownerId) {
+        throw new Error("Database not found");
+    }
+    const getStatisticsResult = await getGeneralDatabaseStatistics(statistics.databaseName);
+
+    try {
+        const client = await clientPromise;
+        const db = client.db();
+
+        if (!getStatisticsResult) {
+            // general statistics don't exist for this database, create new statistics
+            await db.collection(collectionName).insertOne(statistics);
+
+            return;
+        }
+        // general statistics exist for this database
+        if (statistics.updated.getTime() > getStatisticsResult.updated.getTime()) {
+            // update if the latest write event is newer
+            await db.collection(collectionName).updateOne({ databaseName: statistics.databaseName }, { $set: statistics });
+
+            return;
+        }
+        // don't update otherwise
+
+    } catch (error) {
+        throw new Error("Error adding general database statistics");
+    }
 };
