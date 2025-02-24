893ad9f1241597eb5510d00b85519e14ec989d64
lika85456 <lika85456@post.cz>
2025-02-24 10:42:57 +0100

    feat: infrastructure removing script

--- bin/removeInfra.ts
+++ bin/removeInfra.ts
@@ -1,17 +1,27 @@
 import { S3 } from "@aws-sdk/client-s3";
 
+const region = "eu-central-1";
+
 const s3 = new S3({
-    region: "eu-central-1",
+    region,
 });
 
 (async ()=>{
     const buckets = await s3.listBuckets({});
+    console.log(buckets);
 
-    const bucketsToDelete = buckets.Buckets?.map(bucket => bucket.Name).filter(name => name.startsWith("multiverse-test-changes"));
+    const bucketsToDelete = buckets.Buckets?.map(bucket => bucket.Name).filter(name => (
+        name?.startsWith("multiverse-test-changes") ||
+        name?.endsWith("-snapshot") ||
+        name?.startsWith("multiverse-build") ||
+        name?.startsWith("multiverse-snapshot") ||
+        name?.startsWith("mv")
+        ));
     console.log(bucketsToDelete);
 
     if (bucketsToDelete) {
         for (const bucket of bucketsToDelete) {
+            try{
             // first clear the bucket
             const objects = await s3.listObjectsV2({ Bucket: bucket });
             if (objects.Contents) {
@@ -22,6 +32,11 @@ const s3 = new S3({
 
             // then delete the bucket
             await s3.deleteBucket({ Bucket: bucket });
+            }
+            catch(e) {
+                console.log(`cannot delete bucket ${bucket}`);
+                console.log(e);
+            }
         }
     }
 
